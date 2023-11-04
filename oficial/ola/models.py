41e168056477dbc2b664c5e43271db7d5d5b6d77
cataa136 <cperez@usm.cl>
2023-11-03 21:17:27 -0300

    ultimo

--- oficial/ola/models.py
+++ oficial/ola/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create myour models here.
-class curso(models.Model):
+
+class Cliente(models.Model):
     nombre=models.CharField(max_length=30)
-    creditos=models.PositiveSmallIntegerField()
-    
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.CharField(max_length=8)
+class Articulo(models.Model):
+    nombre= models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+class Pedido(models.Model):
+    numero= models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
